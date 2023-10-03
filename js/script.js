cd4ad916d2cf47cfa01ba16279d9abc806dab710
infobijoy <infobijoychandradas@gmail.com>
2023-10-03 21:00:27 +0600

    update counter

--- js/script.js
+++ js/script.js
@@ -1,23 +1,32 @@
 let currentCount = 0;
-let maxCount = 7; // Initial max count for the first radio button
+let maxCount = 6; // Initial max count for the first radio button
 
 function count() {
-  if (document.querySelector('input[name="counter"]:checked')) {
-    let selectedValue = document.querySelector('input[name="counter"]:checked').value;
-    
-    if (selectedValue === "0-7") {
-      maxCount = 6;
-    } else if (selectedValue === "0-33") {
-      maxCount = 32;
-    } else if (selectedValue === "0-99") {
-      maxCount = 98;
-    } else if (selectedValue === "0-99-unlimited") {
-      maxCount = Infinity;
-    }
+    if (document.querySelector('input[name="counter"]:checked')) {
+        let selectedValue = document.querySelector('input[name="counter"]:checked').value;
 
-    currentCount = (currentCount + 1) % (maxCount + 1);
+        if (selectedValue === "0-7") {
+            maxCount = 6;
+        } else if (selectedValue === "0-33") {
+            maxCount = 32;
+        } else if (selectedValue === "0-99") {
+            maxCount = 98;
+        } else if (selectedValue === "0-99-unlimited") {
+            maxCount = Infinity;
+        }
+
+        currentCount = (currentCount + 1) % (maxCount + 1);
+        document.getElementById("count-display").textContent = "Count: " + currentCount;
+
+        if (document.getElementById("vibrationCheckbox").checked) {
+            // Vibrate for 100ms
+            window.navigator.vibrate(100);
+        }
+    } else {
+        console.log("Please select a counting range first.");
+    }
+}
+function resetCount() {
+    currentCount = 0;
     document.getElementById("count-display").textContent = "Count: " + currentCount;
-  } else {
-    console.log("Please select a counting range first.");
-  }
 }
